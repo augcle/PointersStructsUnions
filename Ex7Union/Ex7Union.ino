a38f24ca9ab21a6a2f82e9014016844be169f7af
augcle <augclemmensen@gmail.com>
2026-01-09 12:20:17 +0100

    Added comments to ex7

--- Ex7Union/Ex7Union.ino
+++ Ex7Union/Ex7Union.ino
@@ -1,4 +1,4 @@
-union Dataform {  // Defines Union
+union Dataform {  // Defines a union structure called Dataform
   int intnumb;
   float decnum;
   char letter;
@@ -8,21 +8,20 @@ void setup() {
   Serial.begin(115200);
   delay(2000);
 
-  union Dataform Data;
+  union Dataform Data; // Create a union called data of the structure 'Dataform'
 
-  Data.intnumb = 2;
-  Serial.println(Data.intnumb); 
+  Data.intnumb = 2; // Set the variable intnumb in the union Data
+  Serial.println(Data.intnumb);  // Print the value intnumb
 
   Serial.println("Changing to float"); 
-  Data.decnum = 3.8;
+  Data.decnum = 3.8; // Set the variable decnum in the union Data
   
   Serial.println("The previous part now holds garbage such as:"); 
-  Serial.println(Data.intnumb);
+  Serial.println(Data.intnumb); // The  current value for intnumb
   Serial.println("While the float value holds:"); 
-  Serial.println(Data.decnum);  
-}
+  Serial.println(Data.decnum);  // The value for decnum
+} // The whole point of this is to show that union only holds one variable at a time
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+// It's empty here...
 }
