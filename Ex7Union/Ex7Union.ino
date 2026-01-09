36bd7a5785ad1a0b9563344bd897dd78a21f1011
augcle <augclemmensen@gmail.com>
2026-01-09 11:47:03 +0100

    Created ex7

---
+++ Ex7Union/Ex7Union.ino
@@ -0,0 +1,28 @@
+union Dataform {
+  int intnumb;
+  float decnum;
+  char letter;
+};
+
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+
+  union Dataform Data;
+
+  Data.intnumb = 2;
+  Serial.println(Data.intnumb); 
+
+  Serial.println("Changing to float"); 
+  Data.decnum = 3.8;
+  
+  Serial.println("The previous part now holds garbage such as:"); 
+  Serial.println(Data.intnumb);
+  Serial.println("While the float value holds:"); 
+  Serial.println(Data.decnum);  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
